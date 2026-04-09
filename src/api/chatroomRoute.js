05a7cb73d0a28a3996f1f442dfb2463d6aa2a9cc
reverse-smartass <nguyenminhdore@gmail.com>
2026-04-09 17:33:59 -0400

    commit

--- src/api/chatroomRoute.js
+++ src/api/chatroomRoute.js
@@ -4,7 +4,6 @@ import { body, validationResult } from "express-validator";
 import passport from "passport";
 const chatroomRouter = Router();
 
-
 const validateChatroom = [
   body("name")
     .trim()
@@ -12,11 +11,10 @@ const validateChatroom = [
     .withMessage("Chatroom name cannot be empty")
     .isLength({ min: 3, max: 32 })
     .withMessage("Name must be between 3 and 32 characters")
-    .escape()
+    .escape(),
 ];
 
 const isChatroomOwner = async (req, res, next) => {
-
   const chatroomId = req.params.chatroomid;
 
   try {
@@ -41,199 +39,247 @@ const isChatroomOwner = async (req, res, next) => {
   }
 };
 
-
-chatroomRouter.post("/new", passport.authenticate("jwt", { session: false }), validateChatroom,
+chatroomRouter.post(
+  "/new",
+  passport.authenticate("jwt", { session: false }),
+  validateChatroom,
   async (req, res, next) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.json({
-      errors: errors.array(),
-      previousData: req.body,
-    });
-  }
-
-  const {displayName, users} = req.body;
-  
-
-  try {
-
-    const chatroom = await prisma.chatroom.create({
-      data: {
-        name: displayName,
-        users: {
-          connect: users.map((userId) => ({ id: userId })),
-        },
-        ownerId: req.user.id
-      }
-    });
-    console.log("Created chatroom:", chatroom);
-    res.status(201).json({
-      message: "chatroom created",
-      chatroom: { id: chatroom.id, name: chatroom.name},
-    });
-  } catch (err) {
-    return next(err);
-  }
-  
-});
-
-chatroomRouter.patch("/:chatroomid/edit", passport.authenticate("jwt", { session: false }), validateChatroom, isChatroomOwner, 
-  async (req, res, next) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.json({
-      errors: errors.array(),
-      previousData: req.body,
-    });
-  }
-
-  const chatroomId = req.params.chatroomid;
-  const {displayName, users} = req.body;
-  
-
-  try {
-
-    const updatedchatroom = await prisma.chatroom.update({
-      where: {
-        id: chatroomId,
-      },
-      data: {
-        name: displayName,
-        users: {
-          set: users.map((userId) => ({ id: userId })),
-        },
-      }
-    });
-    console.log("Updated chatroom:", updatedchatroom);
-    res.status(200).json({
-      chatroom: "chatroom updated",
-      updatedchatroom
-    });
-  } catch (err) {
-    return next(err);
-  }
-  
-});
-
-chatroomRouter.patch("/:chatroomid/adduser/:userid", passport.authenticate("jwt", { session: false }), validateChatroom, isChatroomOwner, 
-  async (req, res, next) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.json({
-      errors: errors.array(),
-      previousData: req.body,
-    });
-  }
-
-  const chatroomId = req.params.chatroomid;
-  const userId = req.params.userid;
-  
-  try {
-    const newUser = await prisma.user.findUnique({
-      where: {
-        id: userId
-      }
-    });
-
-    if (!newUser) {
-      return res.status(404).json({ error: "User not found" });
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.json({
+        errors: errors.array(),
+        previousData: req.body,
+      });
     }
 
+    const { displayName, users } = req.body;
 
-    const updatedchatroom = await prisma.chatroom.update({
-      where: {
-        id: chatroomId,
-      },
-      data: {
-        users: {
-          connect: newUser ? { id: newUser.id } : undefined,
+    try {
+      const chatroom = await prisma.chatroom.create({
+        data: {
+          name: displayName,
+          users: {
+            connect: users.map((userId) => ({ id: userId })),
+          },
+          ownerId: req.user.id,
         },
-      }
-    });
-    console.log("Updated chatroom:", updatedchatroom);
-    res.status(200).json({
-      message: "chatroom updated",
-      updatedchatroom
-    });
-  } catch (err) {
-    return next(err);
-  }
-  
-});
-
-chatroomRouter.patch("/:chatroomid/removeuser/:userid", passport.authenticate("jwt", { session: false }), validateChatroom, isChatroomOwner, 
-  async (req, res, next) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.json({
-      errors: errors.array(),
-      previousData: req.body,
-    });
-  }
-
-  const chatroomId = req.params.chatroomid;
-  const userId = req.params.userid;
-  
-  try {
-    const currentUsers = await prisma.user.findMany({
-      where: {
-        chatrooms: {
-          some: {
-            id: chatroomId,
-          }
-        }
-      }
-    });
-
-    const updatedUsers = currentUsers.filter((user) => user.id !== userId).map((user) => ({ id: user.id }));
-
-    const updatedchatroom = await prisma.chatroom.update({
-      where: {
-        id: chatroomId,
-      },
-      data: {
-        users: {
-          set: updatedUsers,
+        include: {
+          users: {
+            select: {
+              id: true,
+              displayName: true,
+              identifier: true,
+            },
+          },
         },
-      }
-    });
-    console.log("Updated chatroom:", updatedchatroom);
-    res.status(200).json({
-      message: "chatroom updated",
-      updatedchatroom
-    });
-  } catch (err) {
-    return next(err);
-  }
-  
-});
-
-chatroomRouter.delete("/:chatroomid/delete", passport.authenticate("jwt", { session: false }), isChatroomOwner, 
-  async (req, res, next) => {
-
-  const chatroomId = req.params.chatroomid;
-
-  try {
-
-    const deletedchatroom = await prisma.chatroom.delete({
-      where: {
-        id: chatroomId,
-      },
-    });
-    console.log("Deteted chatroom:", deletedchatroom);
-    res.status(200).json({
-      chatroom: "chatroom deleted",
-      deletedchatroom
-    });
-  } catch (err) {
-    if (err.code === 'P2025') {
-      res.status(404).json({ error: "User not found" });
-    } else {
-      res.status(500).json({ error: "Something went wrong" });
+      });
+      console.log("Created chatroom:", chatroom);
+      res.status(201).json({
+        message: "chatroom created",
+        chatroom: { id: chatroom.id, name: chatroom.name },
+      });
+    } catch (err) {
+      return next(err);
     }
-  }
-  
-});
+  },
+);
+
+chatroomRouter.patch(
+  "/:chatroomid/edit",
+  passport.authenticate("jwt", { session: false }),
+  validateChatroom,
+  isChatroomOwner,
+  async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.json({
+        errors: errors.array(),
+        previousData: req.body,
+      });
+    }
+
+    const chatroomId = req.params.chatroomid;
+    const { displayName, users } = req.body;
+
+    try {
+      const updatedchatroom = await prisma.chatroom.update({
+        where: {
+          id: chatroomId,
+        },
+        data: {
+          name: displayName,
+          users: {
+            set: users.map((userId) => ({ id: userId })),
+          },
+        },
+        include: {
+          users: {
+            select: {
+              id: true,
+              displayName: true,
+              identifier: true,
+            },
+          },
+        },
+      });
+      console.log("Updated chatroom:", updatedchatroom);
+      res.status(200).json({
+        chatroom: "chatroom updated",
+        updatedchatroom,
+      });
+    } catch (err) {
+      return next(err);
+    }
+  },
+);
+
+chatroomRouter.patch(
+  "/:chatroomid/adduser/:userid",
+  passport.authenticate("jwt", { session: false }),
+  validateChatroom,
+  isChatroomOwner,
+  async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.json({
+        errors: errors.array(),
+        previousData: req.body,
+      });
+    }
+
+    const chatroomId = req.params.chatroomid;
+    const userId = req.params.userid;
+
+    try {
+      const newUser = await prisma.user.findUnique({
+        where: {
+          id: userId,
+        },
+      });
+
+      if (!newUser) {
+        return res.status(404).json({ error: "User not found" });
+      }
+
+      const updatedchatroom = await prisma.chatroom.update({
+        where: {
+          id: chatroomId,
+        },
+        data: {
+          users: {
+            connect: newUser ? { id: newUser.id } : undefined,
+          },
+        },
+        include: {
+          users: {
+            select: {
+              id: true,
+              displayName: true,
+              identifier: true,
+            },
+          },
+        },
+      });
+      console.log("Updated chatroom:", updatedchatroom);
+      res.status(200).json({
+        message: "chatroom updated",
+        updatedchatroom,
+      });
+    } catch (err) {
+      return next(err);
+    }
+  },
+);
+
+chatroomRouter.patch(
+  "/:chatroomid/removeuser/:userid",
+  passport.authenticate("jwt", { session: false }),
+  validateChatroom,
+  isChatroomOwner,
+  async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.json({
+        errors: errors.array(),
+        previousData: req.body,
+      });
+    }
+
+    const chatroomId = req.params.chatroomid;
+    const userId = req.params.userid;
+
+    try {
+      const currentUsers = await prisma.user.findMany({
+        where: {
+          chatrooms: {
+            some: {
+              id: chatroomId,
+            },
+          },
+        },
+      });
+
+      const updatedUsers = currentUsers
+        .filter((user) => user.id !== userId)
+        .map((user) => ({ id: user.id }));
+
+      const updatedchatroom = await prisma.chatroom.update({
+        where: {
+          id: chatroomId,
+        },
+        data: {
+          users: {
+            set: updatedUsers,
+          },
+        },
+        include: {
+          users: {
+            select: {
+              id: true,
+              displayName: true,
+              identifier: true,
+            },
+          },
+        },
+      });
+      console.log("Updated chatroom:", updatedchatroom);
+      res.status(200).json({
+        message: "chatroom updated",
+        updatedchatroom,
+      });
+    } catch (err) {
+      return next(err);
+    }
+  },
+);
+
+chatroomRouter.delete(
+  "/:chatroomid/delete",
+  passport.authenticate("jwt", { session: false }),
+  isChatroomOwner,
+  async (req, res, next) => {
+    const chatroomId = req.params.chatroomid;
+
+    try {
+      const deletedchatroom = await prisma.chatroom.delete({
+        where: {
+          id: chatroomId,
+        },
+      });
+      console.log("Deteted chatroom:", deletedchatroom);
+      res.status(200).json({
+        chatroom: "chatroom deleted",
+        deletedchatroom,
+      });
+    } catch (err) {
+      if (err.code === "P2025") {
+        res.status(404).json({ error: "User not found" });
+      } else {
+        res.status(500).json({ error: "Something went wrong" });
+      }
+    }
+  },
+);
 
 chatroomRouter.get("/", async (req, res) => {
   const result = await prisma.chatroom.findMany();
@@ -260,13 +306,12 @@ chatroomRouter.get("/:chatroomid/users", async (req, res) => {
       chatrooms: {
         some: {
           id: chatroomId,
-        }
-      }
-    }
+        },
+      },
+    },
   });
 
   res.json(result);
 });
-
 
 export default chatroomRouter;
