e6038cc555fdd67702bf624a3fe966857f128415
reverse-smartass <nguyenminhdore@gmail.com>
2026-05-05 20:53:14 -0400

    commit

--- api/app.js
+++ api/app.js
@@ -111,4 +111,8 @@ app.use((err, req, res, _next) => {
   res.status(500).json({ error: "Internal server error" });
 });
 
-app.listen(5000, () => console.log("Server listening on port 5000"));
+if (process.env.NODE_ENV !== "production") {
+  app.listen(5000, () => console.log("Server listening on port 5000"));
+}
+
+export default app;
